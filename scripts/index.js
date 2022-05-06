aa99cc7ca9c1e27647eb7364eccfd4a00864ef3c
Ryzzyie Vickène <jayvickene@gmail.com>
2022-05-06 09:47:34 +0200

    animation part1 terminer

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,21 @@
-var btn2 = document.getElementById('btn');
+//EVENT HOVER SUR LES BOUTONS ADVERSAIRES
+var btnAdversaire = document.getElementsByClassName('btnAdversaire');
+for (let i = 0; i < btnAdversaire.length; i++) {
+    btnAdversaire[i].addEventListener('mouseover', () => {
+        btnAdversaire[i].style.backgroundColor = '#b11313';
+        btnAdversaire[i].style.color = 'white';
+        btnAdversaire[i].style.fontWeight = 'bold';
+        btnAdversaire[i].style.boxShadow = '5px 5px 20px white'
+    })
+    btnAdversaire[i].addEventListener('mouseleave', () => {
+        btnAdversaire[i].style.backgroundColor = 'white';
+        btnAdversaire[i].style.color = '#b11313';
+        btnAdversaire[i].style.boxShadow = 'none';
+    })
 
-function myFunction() {
-    btn2.style.backgroundColor = '#fff'
-    btn2.style.color = '#b11313'
 }
 
-function myFunction2() {
-    btn2.style.backgroundColor = 'rgba(0, 0, 0, 0)'
-    btn2.style.color = '#fff'
-}
+//Fonction du Timer
 var countDownDate = new Date("May 4, 2022 08:00:00").getTime();
 var x = setInterval(function() {
     var now = new Date().getTime();
@@ -28,4 +35,34 @@ var x = setInterval(function() {
 }, 1000)
 
 
-document.getElementById('para').innerHTML = "<B>1225225</B>"
+//EVENT HOVER SUR L'HEADER
+var lienHeader = document.querySelectorAll('li');
+for (let i = 0; i < lienHeader.length; i++) {
+    lienHeader[i].addEventListener('mouseover', () => {
+        lienHeader[i].style.textDecoration = 'line-through';
+    })
+    lienHeader[i].addEventListener('mouseleave', () => {
+        lienHeader[i].style.textDecoration = 'none';
+    })
+}
+
+//EVENT HOVER SUR ICONE RESEAUX SOCIAUX FOOTER
+var iconeFooter = document.getElementsByClassName('icone-footer');
+for (let i = 0; i < iconeFooter.length; i++) {
+    iconeFooter[i].addEventListener('mouseover', () => {
+        iconeFooter[i].style.color = 'white';
+    })
+    iconeFooter[i].addEventListener('mouseleave', () => {
+        iconeFooter[i].style.color = 'black';
+    })
+}
+var titreH = document.querySelectorAll('h1');
+console.log(titreH)
+
+
+$(document).ready(function() {
+    //ANIMATION FADEIN DE TOUS LES TITRES AVEC LA FONT FAR-FROM-HOME-COMING ET LE SOUS TITRE
+    $('h1').animate({ left: '5px' }, 6000);
+    $('.description').fadeIn(6000);
+    $('h3').fadeIn(6000);
+});
